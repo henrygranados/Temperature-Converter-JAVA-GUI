78b6ab1283c9d9390059780f8a7497953c90acf4
Henry Granados <henrygranados03@gmail.com>
2016-04-28 20:22:51 -0600

    added new functionality to write user's input to a file

--- src/Temperature.java
+++ src/Temperature.java
@@ -4,13 +4,14 @@
  * Website: http://henrygranados.github.io/
  */
 import javax.swing.JFrame;
+
 public class Temperature {
 	public static void main(String[] args) {
-		
+
 		TemperatureConverter temperatureConverter = new TemperatureConverter();
-		temperatureConverter.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-		temperatureConverter.setSize (500, 300);
+		temperatureConverter.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		temperatureConverter.setSize(500, 300);
 		temperatureConverter.setLocation(500, 400);
-		temperatureConverter.setVisible (true); 
+		temperatureConverter.setVisible(true);
 	}
 }
--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -9,42 +9,50 @@ import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+
 import javax.swing.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 import javax.swing.JButton;
 
 public class TemperatureConverter extends JFrame {
 
-	JButton FahToCelButton;
-	JButton CelToFahButton;
-	JLabel labelFahrenheit;
-	JLabel labelCelsius;
-	JTextField FahrTextField;
-	JTextField CelsTextField;
+	JButton FahToCelButton; // Fahrenheit converter button
+	JButton CelToFahButton; // Celsius converter button
+	JLabel labelFahrenheit; // Fahrenheit label
+	JLabel labelCelsius;    // Celsius label
+	JTextField FahrTextField;// Fahrenheit textfield
+	JTextField CelsTextField;// Celsius textfield
 
+	/**
+	 * Constructor of TemperatureConverter class
+	 */
 	public TemperatureConverter() {
 
-		super("Temperature Converter");
+		super("Temperature Converter"); // Sets the title of the GUI
 		setLayout(new FlowLayout());
-		labelFahrenheit = new JLabel("Temperature in °F");
+		labelFahrenheit = new JLabel("Temperature in °F");// Sets Fahrenheit label
 		add(labelFahrenheit);
-		FahrTextField = new JTextField(14);
+		FahrTextField = new JTextField(10);
 		add(FahrTextField);
 
-		labelCelsius = new JLabel("Temperature in °C");
+		labelCelsius = new JLabel("Temperature in °C");// Sets Celsius label
 		add(labelCelsius);
-		CelsTextField = new JTextField(14);
+		CelsTextField = new JTextField(10);
 		add(CelsTextField);
 
-		FahToCelButton = new JButton("Convert °F to °C");
+		FahToCelButton = new JButton("Convert °F to °C");// Text on Fahrenheit button
 		FahToCelButton.setForeground(Color.BLACK);
 		FahToCelButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
 		add(FahToCelButton);
-		CelToFahButton = new JButton("Convert °C to °F");
+		CelToFahButton = new JButton("Convert °C to °F");// Text on Celsius button
 		CelToFahButton.setForeground(Color.BLACK);
-		CelToFahButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
-		add(CelToFahButton);
+		CelToFahButton.setCursor(new Cursor(Cursor.HAND_CURSOR)); // cursor to button
+		add(CelToFahButton);		
 
 		JPanel panel = new JPanel(new GridLayout(2, 2, 20, 20));
 		panel.add(labelFahrenheit);
@@ -61,17 +69,32 @@ public class TemperatureConverter extends JFrame {
 		FahToCelButton.addActionListener(new fahrenheitConverterListener());
 		CelToFahButton.addActionListener(new celsConverterListener());
 	}
-
+		
+	/**
+	 * fahrenheitConverterListener Class
+	 * Here we add actual functionality to the fahrenheit button
+	 *
+	 */
 	private class fahrenheitConverterListener implements ActionListener {
+		
+		/**
+		 * This method will validate user's input and ensure input is 
+		 * an valid number otherwise the isUsersInputANumber method will catch it
+		 * by outputting a message on the console and the actual GUI.
+		 * This method will also process user's input and do a math conversion to Celsius degrees
+		 */
 		public void actionPerformed(ActionEvent event) {
 			double degrees = 0;
 			if (event.getSource() == FahToCelButton) {
 				if (isUsersInputANumber(FahrTextField.getText())) {
 					degrees = Double.parseDouble(FahrTextField.getText());
 					int conFahToCel = (int) ((5.0 / 9.0 * (degrees - 32)));
-					CelsTextField.setText(conFahToCel + " °C");
+					CelsTextField.setText(String.valueOf(conFahToCel));
 					FahrTextField.requestFocus();
 					FahrTextField.selectAll();
+					String firstText = FahrTextField.getText();
+					String secondText = CelsTextField.getText();
+					writingToFile(firstText, secondText);
 				}
 
 				else if (!isUsersInputANumber(FahrTextField.getText())) {
@@ -81,6 +104,12 @@ public class TemperatureConverter extends JFrame {
 		}
 	}
 
+	/**
+	 * This method will validate user's input and ensure input is 
+	 * an valid number otherwise the isUsersInputANumber method will catch it
+	 * by outputting a message on the console and the actual GUI.
+	 * This method will also process user's input and do a math conversion to fahrenheit degrees
+	 */
 	private class celsConverterListener implements ActionListener {
 		public void actionPerformed(ActionEvent event) {
 			double degrees = 0;
@@ -88,9 +117,12 @@ public class TemperatureConverter extends JFrame {
 				if (isUsersInputANumber(CelsTextField.getText())) {
 					degrees = Double.parseDouble(CelsTextField.getText());
 					int conCelToFah = (int) ((9.0 / 5.0 * (degrees + 32)));
-					FahrTextField.setText(conCelToFah + " °F");
+					FahrTextField.setText(String.valueOf(conCelToFah));
 					CelsTextField.requestFocus();
 					CelsTextField.selectAll();
+					String firstText = FahrTextField.getText();
+					String secondText = CelsTextField.getText();
+					writingToFile(firstText, secondText);
 				} else if (!isUsersInputANumber(CelsTextField.getText())) {
 					CelsTextField.setText("Error: Only numbers");
 				}
@@ -99,18 +131,37 @@ public class TemperatureConverter extends JFrame {
 	}
 
 	/**
-	 * Method that checks if user's input is a number or not
+	 * This method checks if user's input is a valid number or not
 	 * 
-	 * @param degrees
-	 * @return
+	 * @param degrees degrees entered by user
+	 * @return true if degrees is a valid number otherwise returns false if it's not
 	 */
 	public static boolean isUsersInputANumber(String degrees) {
 		try {
 			double d = Double.parseDouble(degrees);
 		} catch (NumberFormatException ex) {
-			ex.printStackTrace();
+			System.out.println("Error: Only numbers");
 			return false;
 		}
 		return true;
 	}
+	
+	/**
+	 * This method will write user's input to file
+	 * @param firstText
+	 * @param secondText
+	 */
+	public static void writingToFile(String firstText, String secondText){
+		
+		try {
+			String myString = firstText + " °F" + "..........." + secondText + " °C";
+			String filename = "file.txt";
+			FileWriter myfile = new FileWriter(filename, true); // the true will append the new data
+			myfile.write(myString + "\t\t\t\t\n");// appends the string to the file
+			myfile.close();
+		} catch (Exception ex) {
+			System.out.println("An error has occurred writing to the file");
+		}
+	}	
+	
 }
